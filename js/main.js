46ccda67077840c856ee739b133810002f529ae9
marialemon <maria.nc@hotmail.es>
2021-05-12 11:06:30 +0200

    add js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,45 @@
 //Constantes
 
 const buttonSubmit = document.querySelector(".js-button");
-const input = document.querySelector(".js-input");
-const inputValue = input.value;
+const inputNumber = document.querySelector(".js-input");
+const inputNumberValue = inputNumber.value; //aquí no entiendo por qué no me pilla esta variable
+const randomNumber = Math.ceil(Math.random() * 100);
 
 //Funciones
 
 const getRandomNumber = () => {
-  let randomNumber = Math.ceil(Math.random() * 100);
   console.log(`El número aleatorio es: ${randomNumber}`);
 };
 
-// for (let i = 0; i < array.length; i++) {
-//  const element = array[i];
-// }
+const checkNumber = () => {
+  //Crear div
+  let div = document.createElement("div");
+  div.classList.add("message");
+  const form = document.querySelector(".form");
+
+  //Añadir el div al formulario
+  form.appendChild(div);
+
+  //Validar
+  if (inputNumber.value === "") {
+    div.innerHTML = "El número debe estar entre 1 y 100";
+  } else if (inputNumber.value > randomNumber) {
+    div.innerHTML = "Demasiado alto";
+  } else if (inputNumber.value < randomNumber) {
+    div.innerHTML = "Demasiado bajo";
+  } else {
+    div.innerHTML = `¡Has acertado!`;
+  }
+};
+
+const handlerClickButton = () => {
+  const arrayNumbers = [];
+  arrayNumbers.push(inputNumber.value);
+
+  console.log(arrayNumbers);
+
+  checkNumber();
+};
 
 //Declarar funciones
 
@@ -21,4 +47,4 @@ getRandomNumber();
 
 //Eventos
 
-buttonSubmit.addEventListener("click", getRandomNumber);
+buttonSubmit.addEventListener("click", handlerClickButton);
