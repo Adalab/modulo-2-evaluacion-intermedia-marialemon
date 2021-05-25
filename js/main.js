4fb205d17a878e9b94a8dc4761dd326eb1e5fe39
marialemon <maria.nc@hotmail.es>
2021-05-25 10:48:09 +0200

    fix issues

--- js/main.js
+++ js/main.js
@@ -20,16 +20,21 @@ const checkNumber = () => {
   //Añadir el div al formulario
   form.appendChild(p);
 
+  let message = "";
+
   //Validar
   if (!inputNumber.value || inputNumber.value > 100) {
-    p.innerHTML = "El número debe estar entre 1 y 100";
+    message = `El número debe estar entre 1 y 100`;
   } else if (inputNumber.value > randomNumber) {
-    p.innerHTML = "Demasiado alto";
+    message = "Demasiado alto";
   } else if (inputNumber.value < randomNumber) {
-    p.innerHTML = "Demasiado bajo";
+    message = "Demasiado bajo";
   } else {
-    p.innerHTML = `¡Has acertado!`;
+    message = `¡Has acertado!`;
   }
+
+  let $text = document.createTextNode(message);
+  p.appendChild($text);
 };
 
 const counterMessage = () => {
@@ -38,7 +43,8 @@ const counterMessage = () => {
   const form = document.querySelector(".form");
   form.appendChild(p);
 
-  p.innerHTML = `Número de intentos ${acc}`;
+  const $text = document.createTextNode(`Número de intentos ${acc}`);
+  p.appendChild($text);
 };
 
 let acc = 0;
@@ -46,14 +52,34 @@ const addCounter = () => {
   acc += 1;
   const arrayNumbers = [];
   arrayNumbers.push(inputNumber.value);
-
+  cleanCounter();
   counterMessage();
 };
 
-const handlerClickButton = () => {
+function cleanCounter() {
+  //Seleccionar las cards para poder eliminarlas
+  const $counterMessage = document.querySelectorAll(".counter__message");
+
+  for (const p of $counterMessage) {
+    p.remove();
+  }
+}
+
+function cleanValidation() {
+  //Seleccionar las cards para poder eliminarlas
+  const $ValidationMessage = document.querySelectorAll(".message");
+
+  for (const p of $ValidationMessage) {
+    p.remove();
+  }
+}
+
+function handlerClickButton(ev) {
+  ev.preventDefault();
   addCounter();
+  cleanValidation();
   checkNumber();
-};
+}
 
 //Eventos
 
